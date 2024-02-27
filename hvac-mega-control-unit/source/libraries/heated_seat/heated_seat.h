9ff5ec0507bdd6f68a75d2841c629273cd32fce6
ciaranowens <hello@ciaranowens.com>
2024-02-27 19:08:32 +0000

    Update heated_seat.h
    Add clarity to the comments of the functions

--- hvac-mega-control-unit/source/libraries/heated_seat/heated_seat.h
+++ hvac-mega-control-unit/source/libraries/heated_seat/heated_seat.h
@@ -19,19 +19,21 @@ class HeatedSeat {
     public:
         HeatedSeat(byte OutputPin, byte NumTempLevels, boolean PWMControl, byte InputPin);
         // OutputPin is Heated Seat Output (PWM)
+        // NumTempLevels (BUTTONS ONLY) is how many temperature levels are desired.
+        // PWMControl is if native PWM control from a rotary dial input is being used
+        // InputPin is which pin should be used to read the rotary dial input from
 
         void init();
         // Setup the pins and set default state
 
         void poll();
-        // Poll the input and set the output (for PWM applications)
+        // Poll the input or calculate the DesiredOutput and set the output
 
         void increaseTemp();
-        // Increase the temperature
+        // Increase the temperature by 1 out of the NumTempLevels
 
         void decreaseTemp();
-        // Decrease the temperature
-
+        // Decrease the temperature by 1 out of the NumTempLevels
 }
 
 
